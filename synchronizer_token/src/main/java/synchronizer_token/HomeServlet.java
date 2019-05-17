4d4e586d57d18af109639d82bd3776d43bb1ac77
dinuZgit <dinusdjay@gmail.com>
2019-05-17 23:53:20 +0530

    home servlet

---
+++ synchronizer_token/src/main/java/synchronizer_token/HomeServlet.java
@@ -0,0 +1,14 @@
+package synchronizer_token;
+
+public class HomeServlet {
+
+	public HomeServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
